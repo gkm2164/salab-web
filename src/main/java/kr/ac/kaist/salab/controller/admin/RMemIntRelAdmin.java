3dedfa11567f3451b308b97a6eb7163c241521c7
Gyeongmin Go <gkm2164@gmail.com>
2016-03-07 14:46:29 +0900

    Member &  Interest relation admin page

---
+++ src/main/java/kr/ac/kaist/salab/controller/admin/RMemIntRelAdmin.java
@@ -0,0 +1,81 @@
+package kr.ac.kaist.salab.controller.admin;
+
+import kr.ac.kaist.salab.controller.page.LayoutController;
+import kr.ac.kaist.salab.controller.page.PageDescription;
+import kr.ac.kaist.salab.model.entity.Interest;
+import kr.ac.kaist.salab.model.entity.Member;
+import kr.ac.kaist.salab.model.entity.RMemberInterest;
+import kr.ac.kaist.salab.model.repository.InterestRepository;
+import kr.ac.kaist.salab.model.repository.MemberRepository;
+import kr.ac.kaist.salab.model.repository.PublicationRepository;
+import kr.ac.kaist.salab.model.repository.RMemberInterestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by gyeongmin on 3/7/16.
+ */
+@Controller
+@RequestMapping("/admin/memintrel")
+public class RMemIntRelAdmin extends LayoutController {
+    @Autowired private InterestRepository ir;
+    @Autowired private MemberRepository mr;
+    @Autowired private RMemberInterestRepository rmir;
+
+    @RequestMapping(value = "new", method = RequestMethod.GET)
+    public String newRelation(Model model) {
+        PageDescription nrPageDesc = new PageDescription(
+                "admin/memintrel", "New Relation",
+                (css, js) -> {
+                }
+        );
+        model.addAttribute("relation", new RMIBox ());
+        model.addAttribute("interests", ir.findAll());
+        model.addAttribute("members", mr.findAll());
+
+        return layoutCall(nrPageDesc, model);
+    }
+
+    @RequestMapping(value = "new", method = RequestMethod.POST)
+    public String newRelation(@ModelAttribute RMIBox rmib, BindingResult br) {
+        if (br.hasErrors()) {
+            return "redirect:/admin/memintrel/new";
+        }
+
+        Interest i = ir.findOne(rmib.intId);
+        Member m = mr.findOne(rmib.memId);
+
+        RMemberInterest rmi = new RMemberInterest();
+        rmi.setInterest(i);
+        rmi.setMember(m);
+
+        rmir.save(rmi);
+        return "redirect:/admin/memintrel/new";
+    }
+
+    public class RMIBox {
+        private Integer intId;
+        private Integer memId;
+
+        public Integer getIntId() {
+            return intId;
+        }
+
+        public void setIntId(Integer intId) {
+            this.intId = intId;
+        }
+
+        public Integer getMemId() {
+            return memId;
+        }
+
+        public void setMemId(Integer memId) {
+            this.memId = memId;
+        }
+    }
+}
--- src/main/webapp/WEB-INF/jsps/admin/memintrel.jsp
+++ src/main/webapp/WEB-INF/jsps/admin/memintrel.jsp
@@ -1,8 +1,18 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: USER
-  Date: 2016-03-07
-  Time: 오후 1:42
-  To change this template use File | Settings | File Templates.
---%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<form:form method="post" modelAttribute="relation">
+  <p>
+    <form:label path="intId">Interest</form:label>
+    <form:select path="intId">
+      <form:option value="Select Interest" />
+      <form:options items="${interests}" itemLabel="fieldName" itemValue="id" />
+    </form:select>
+  </p>
+  <p>
+    <form:label path="memId">Member</form:label>
+    <form:select path="memId">
+      <form:option value="Select Member" />
+      <form:options items="${members}" itemLabel="name" itemValue="id" />
+    </form:select>
+  </p>
+</form:form>
